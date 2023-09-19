74b13eed39d46a1d30cd2fe7634fa1623a3ba829
cclabdis <cclabdis@gmail.com>
2023-09-19 15:46:05 -0300

    aula 2

--- aula-prisma-01/index.ts
+++ aula-prisma-01/index.ts
@@ -1,30 +1,8 @@
-import pg from "pg";
+import prisma from "./database";
 
-const { Pool } = pg;
-
-const connection = new Pool({
-    host: "mahmud.db.elephantsql.com",
-    port: 5432,
-    user: "dyjnkion",
-    password: "qubY9LSv75VLup16dVOKvU5B9ojvW7kF",
-    database: "dyjnkion"
-});
-
-
-type User ={
-    id: number;
-    name: string;
-    cpf: string;
-    phone: string;
-    email: string;
-    password: string;
-    created_at: string;
-}
 async function find() {
-    const users = await connection.query(
-        `SELECT * FROM users
-        `);
-    return users.rows
+    const users = await prisma.clients.findMany();
+    return users;
 }
 
 (async () => {
