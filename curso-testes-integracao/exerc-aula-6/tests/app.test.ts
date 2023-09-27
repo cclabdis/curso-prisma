397a2db103b900b084e9e2a62a8215d92b00d06a
cclabdis <cclabdis@gmail.com>
2023-09-27 16:15:17 -0300

    aula 6 exerc 6

---
+++ curso-testes-integracao/exerc-aula-6/tests/app.test.ts
@@ -0,0 +1,20 @@
+import supertest from "supertest";
+
+import app from "./../src/app";
+
+const api = supertest(app);
+
+describe("API test", () => {
+  it("/numbers", async () => {
+    const result = await api.get("/event")
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toEqual({
+      id: 1,
+      title: "Super Event!",
+      image: "https://img.freepik.com/fotos-gratis/publico-animado-assistindo-fogos-de-artificio-de-confete-e-se-divertindo-no-festival-de-musica-a-noite-copiar-espaco_637285-559.jpg",
+      date: "2023-07-21T00:00:00.000Z"
+    }); 
+     expect(result.body).toMatchObject({title: "Super Event!"});
+   
+});
+});
--- curso-testes-integracao/projeto-base/tests/app.test.ts
+++ curso-testes-integracao/projeto-base/tests/app.test.ts
@@ -1,3 +1,4 @@
+import { array } from "joi";
 import app from "./../src/app";
 import supertest from "supertest";
 
@@ -8,6 +9,27 @@ describe("api", () => {
         const result = await server.get("/health")
         const { statusCode } = result
         expect(statusCode).toBe(200); //matchers
+    });
+
+    it("/numbers", async () => {
+        const result = await server.get("/numbers")
+        expect(result.statusCode).toBe(200);
+        expect(result.body).toHaveLength(5)
+        expect(result.body).toEqual([1, 2 , 3, 4, 5]); //matchers
+        expect(result.body).toContain(3)
+        expect(result.body).toEqual(expect.arrayContaining([1, 3]))
+    });
+
+    //objetos
+    it("/numbers", async () => {
+        const result = await server.get("/numbers/:test")
+        expect(result.statusCode).toBe(200);
+        expect(result.body).toHaveLength(5)
+        expect(result.body).toEqual({estrutura: 0, de: 1, array:2}); 
+        expect(result.body).toEqual({estrutura: 0, de: 1, array: expect.any(Number)});//matchers
+        expect(result.body).toMatchObject({estrutura: 0, de: 1});
+        expect(result.body).toContain(3)
+        expect(result.body).toEqual(expect.arrayContaining([1, 3]))
     });
 
     it("/numbers", async () => {
