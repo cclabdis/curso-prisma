4c51243a75bf0209effcef415e58463da6d577bf
cclabdis <cclabdis@gmail.com>
2023-10-02 22:01:52 -0300

    exercicio 4 + resumo

--- curso-testes-unitarios/aula04/tests/unit/calculator.test.ts
+++ curso-testes-unitarios/aula04/tests/unit/calculator.test.ts
@@ -1,5 +1,36 @@
-describe("calculator tests", () => {
+import calculator from "calculator";
+
+function generateValue(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+describe("calculator unit test suite", () => {
   it("should work", async () => {
-    expect(true).toBe(true);
+    const value1 = generateValue(2, 10);
+    const value2 = generateValue(2, 10);
+    const result = calculator.sum(value1, value2)
+    expect(result).toBe(value1 + value2);
   });
+
+  it("should work", async () => {
+    const value1 = generateValue(2, 10);
+    const value2 = generateValue(2, 10);
+    const result = calculator.div(value1, value2)
+    expect(result).toBe(value1 / value2);
+  });
+
+  it("should work", async () => {
+    const value1 = generateValue(2, 10);
+    const value2 = generateValue(2, 10);
+    const result = calculator.mul(value1, value2)
+    expect(result).toBe(value1 * value2);
+  });
+
+  it("should work", async () => {
+    const value1 = generateValue(2, 10);
+    const value2 = generateValue(2, 10);
+    const result = calculator.sub(value1, value2)
+    expect(result).toBe(value1 - value2);
+  });
+
 })
--- curso-testes-unitarios/resumo.md
+++ curso-testes-unitarios/resumo.md
@@ -1,7 +1,42 @@
 # Curso de testes unitarios
 
 
-
 Testes realizados em camada unitária, especialmente do service.
 
 Nos testes unitarios nao usamos o supertest
+
+*Jest* Configurações:
+
+```json
+npm i -D jest ts-jest typescript @types/jest
+```
+no *js.config.ts*
+
+```javascript
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: ["<rootDir>/tests/*.(test|spec).ts"],
+};
+```
+
+no *tsconfig.json*
+
+```javascript
+{
+  "compilerOptions": {
+    "outDir": "dist",
+    "esModuleInterop": true,
+    "module": "CommonJS",
+    "moduleResolution": "Node",
+    "baseUrl": "src",
+  },
+  "ts-node":{
+    "esm": true
+  },
+}
+```
