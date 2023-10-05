d3e2772bbbaa5a678529d59f5ac50d920b2fc085
cclabdis <cclabdis@gmail.com>
2023-10-05 09:02:33 -0300

    exerc mini curso

--- curso-testes-unitarios/aula05/tests/unit/pacientes.test.ts
+++ curso-testes-unitarios/aula05/tests/unit/pacientes.test.ts
@@ -1,4 +1,5 @@
-import { generateProtocolForPacient } from "protocols-generator";
+import { faker } from "@faker-js/faker";
+import { generateProtocolForPacient } from "../../src/protocols-generator";
 
 jest.mock('uuid', () => ({
   v4: () => 'voucher gerado por mock' //uma string qualquer
@@ -6,13 +7,18 @@ jest.mock('uuid', () => ({
 
 describe("generateProtocolForPacient", () => {
   it("protocol for pacient", async () => {
-    const protocol = generateProtocolForPacient("Juliana", "Alves", true)
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const priority = faker.datatype.boolean()
+
+
+    const protocol = generateProtocolForPacient(firstName, lastName, priority)
     expect(protocol).toEqual({
 
-      priority: true,
-      pacient: "Juliana Alves",
+      priority,
+      pacient: `${firstName} ${lastName}`,
       date: expect.any(Date),
-      protocol: expect.any(String)
+      protocol: 'voucher gerado por mock'
     });
   });
 });
--- curso-testes-unitarios/aula06/tests/unit/orders.test.ts
+++ curso-testes-unitarios/aula06/tests/unit/orders.test.ts
@@ -4,58 +4,27 @@ import { createOrder, getOrderByProtocol } from "../../src/order-service";
 import * as orderRepository from "../../src/order-repository";
 import { OrderInput } from "../../src/validator";
 
-// it("should apply discount", async () => {
-//   const voucher: VoucherCreateData = {
-//     code: "AAA",
-//     discount: 10,
-//     used: false,
-//   };
-
-//   jest
-//     .spyOn(voucherRepository, "getVoucherByCode")
-//     .mockImplementationOnce((): any => {
-//       return {
-//         id: 1,
-//         code: voucher.code,
-//         discount: voucher.discount,
-//         used: voucher.used,
-//       };
-//     });
-
-//   jest
-//     .spyOn(voucherRepository, "useVoucher")
-//     .mockImplementationOnce((): any => {});
-
-//   const amount = 1000;
-//   const order = await voucherService.applyVoucher(voucher.code, amount);
-
-//   expect(order.amount).toBe(amount);
-//   expect(order.discount).toBe(voucher.discount);
-//   expect(order.finalAmount).toBe(
-//     amount - amount * (voucher.discount / 100)
-//   );
-// });
-
 describe("Order Service Tests", () => {
   it("should create an order", async () => {
     const order: OrderInput = {
-      client: "Juju",
-      description: "lalalalal",
-      
+      client: faker.person.fullName(),
+      description: faker.lorem.paragraph(),
     };
-    jest.spyOn(orderRepository, "create").mockImplementation((): any => {
-     return {
-      id: 1,
-      client: order.client,
-      description: order.description,
-      protocol: new Date().getTime().toString(),
-      status: "IN_PREPARATION"
-     }
+    const protocol = "uma string qualquer"
+    const mock = jest.spyOn(orderRepository, "create")
+    mock.mockImplementation((): any => {
+      return {
+        protocol: protocol,
+        status: "IN_PREPARATION"
+      }
     })
-    const orderCall = await createOrder(order)
-    expect(orderCall).toBe(order);
 
-    expect(true).toBe(true);
+    const orderCall = await createOrder(order)
+    expect(orderRepository.create).toBeCalledTimes(1)
+    expect(orderCall).toEqual({
+      protocol,
+      status: "IN_PREPARATION",
+  });
   });
 
   it("should return an order based on the protocol", async () => {
